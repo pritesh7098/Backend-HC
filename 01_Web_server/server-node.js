fee00bc8f22890b90a3bc718afce065d0f313ddb
hey pritesh <priteshdhanad2001@gmail.com>
2024-08-23 21:12:47 +0530

    Server is created with node.js

--- 01_Web_server/server-node.js
+++ 01_Web_server/server-node.js
@@ -3,22 +3,27 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000;
 
+// http server creation
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.statusCode = 200;
-    res.setHeader("content-type", "text/plain")
-    res.end("hello chai")
-  } else if (req.url === '/garam' ){
+    res.setHeader("content-type", "text/plain");
+    res.end("hello chai");
+  } else if (req.url === "/a") {
     res.statusCode = 200;
-    res.setHeader(" content-type", "text/plain")
-    res.end("hello chai but bohot garm hain bhai ")
+    res.setHeader(" content-type", "text/plain");
+    res.end("hello chai but bohot garm hain bhai ");
   } else {
     res.statusCode = 404;
-    res.setHeader(" content-type", "text/plain")
-    res.end("404 NOT FOUND ")
+    res.setHeader(" content-type", "text/plain");
+    res.end("404 NOT FOUND ");
   }
 });
 
+// listing on http://localhost
 server.listen(port, hostname, () => {
+  console.log("Server running successfully");
+
   console.log(`server is listening at http://${hostname}:${port}`);
 });
+
